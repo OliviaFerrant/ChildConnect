16d0a8a372dca701686240bf69830f052a5c7ea8
Louisa Wong <lou.h.wong@gmail.com>
2019-06-04 16:53:19 +0900

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,12 +31,6 @@ ActiveRecord::Schema.define(version: 2019_06_04_073125) do
     t.index ["user_id"], name: "index_cases_on_user_id"
   end
 
-  create_table "risks", force: :cascade do |t|
-    t.string "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

f232e9ce628e47e33ef560ff66af5760a13d474a
Jun Miyazaki <jun.miyazaki918@gmail.com>
2019-06-04 18:09:41 +0900

    changed show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_073125) do
+ActiveRecord::Schema.define(version: 2019_06_04_080329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,15 +26,9 @@ ActiveRecord::Schema.define(version: 2019_06_04_073125) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "case_number"
-    t.string "phone_numbers"
+    t.string "phone_number"
     t.string "summary"
     t.index ["user_id"], name: "index_cases_on_user_id"
-  end
-
-  create_table "risks", force: :cascade do |t|
-    t.string "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

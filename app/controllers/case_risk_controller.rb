3eb4441f4a43ee4210b25fbe51e2cfedc6e12343
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-06-05 12:32:27 +0900

    added all the CRUD actions. Fixed the navbar links and the routes.

--- app/controllers/case_risk_controller.rb
+++ app/controllers/case_risk_controller.rb
@@ -1,26 +1,34 @@
 class CaseRiskController < ApplicationController
     before_action :set_case, only: [:new, :create, :edit, :update]
 
-    def index
+		def index
+			@case_risks = policy_scope(Risk).order(created_at: :desc)
     end
 
     def new
-			@caserisk = Caserisk.new
+			@case_risk = Risk.new
     end
 
     def create
-			@caserisk = Caserisk.new
-			@caserisk.risk = @risk
+			@case_risk = Risk.new
+			@case_risk.risk = @risk
     end
 
 		def edit
 			
     end
 
-    def update
+		def update
+			if @case_risk.update(case_risk_params)
+				redirect_to case_path(@case)
+			else
+				render :edit
+			end
     end
 
-    def destroy
+		def destroy
+			@case_risk.destroy
+			redirect_to case_path(@case)
     end
 
     private
@@ -29,6 +37,6 @@ class CaseRiskController < ApplicationController
 			@case = case.find(params[:case_id])
     end
 
-    def caserisk_params
-			params.require(:caserisk).permit(:risk_id)
+    def case_risk_params
+			params.require(:risk).permit(:risk_id)
 end

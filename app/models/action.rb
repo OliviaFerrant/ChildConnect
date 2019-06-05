45d7c657da45edc920fa45aded0a99a562954538
Louisa Wong <lou.h.wong@gmail.com>
2019-06-05 16:53:53 +0900

    fixed the actions

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,8 +1,8 @@
 class Action < ApplicationRecord
   belongs_to :case
-  validates :due_date, presence: true
-  validates :status, presence: true
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :report, presence: true
+  # validates :due_date, presence: true
+  # validates :status, presence: true
+  # validates :title, presence: true
+  # validates :description, presence: true
+  # validates :report, presence: true
 end

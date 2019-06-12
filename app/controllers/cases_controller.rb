508e24019063815dd641e466527bbae0a3fd5316
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-06-12 18:53:33 +0900

    fixed the display of the urgent cases

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -7,6 +7,10 @@ class CasesController < ApplicationController
     # raise
     @urgent_cases = policy_scope(Case.joins(:case_actions)).where("case_actions.status = 'Pending' AND case_actions.due_date BETWEEN ? AND ?", Date.today, Date.today + 1).order(due_date: :asc)
     @cases = policy_scope(Case).where.not(id: @urgent_cases)
+    @cases = @cases.sort_by do |c|
+      c.next_action.due_date
+    end
+
   end
 
   def show
